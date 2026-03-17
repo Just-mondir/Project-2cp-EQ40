f6ef6547255a3e11dd3068064a254c7b036d02e0
tin <ot_cheballah@esi.dz>
2026-03-17 02:08:27 +0100

    added user pagination containing user info

--- Backend/heritage_backend/apps/moderator/urls.py
+++ Backend/heritage_backend/apps/moderator/urls.py
@@ -5,11 +5,13 @@ from __future__ import annotations
 from django.urls import path
 
 from .views import (
-    PlatformStatsView
+    PlatformStatsView,
+    UserListView
 )
 
 urlpatterns = [
     # Statistics
     path("stats/", PlatformStatsView.as_view(), name="platform-stats"),
+    path("users/", UserListView.as_view(), name="user-list"),
 
 ]
